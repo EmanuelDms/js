214fb8a3dca431e701fc09f6e9126bdefc3609e5
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-12-09 11:54:29 -0300

    feat: add multiple buttons and squares

--- w3schools/2 - intro/showElements/script.js
+++ w3schools/2 - intro/showElements/script.js
@@ -1,15 +1,34 @@
 const SHOW = 1;
 const HIDE = 0;
 
+/**
+ * * Ao clicar em um quadrado, o mesmo fica selecionado
+ */
+function handleSquaresClick(e) {
+  const [selectedSquare] = e.path;
+  const isSquareClicked = selectedSquare.style.border !== 'none';
+  if (!isSquareClicked) {
+    selectedSquare.style.border = '1px solid black';    
+  };
+
+  return selectedSquare;
+}
+
 function defaultSettings() {
   let squares = document.getElementsByClassName('square');
   Object.entries(squares).forEach(([, square], index) => {
+    square.style.border = 'none';
     return square.style.display = 'inline-block';
   });
   let handleVisibilityButton = document.getElementById('handleVisibilityButton');
   handleVisibilityButton.innerHTML = 'Ocultar quadrado';
   handleVisibilityButton.value = SHOW;
 };
+
+/**
+ * * Ao clicar em ocultar, somente ele ser afetado.
+ * * Após isso, ser criado botão de mostrar todos
+ */
 
 defaultSettings();
 
@@ -26,4 +45,12 @@ handleVisibilityButton.onclick = () => {
     handleVisibilityButton.value = HIDE;
     handleVisibilityButton.innerHTML = 'Mostrar quadrado';
   } else defaultSettings();
+};
+
+let squaresContainer = document.getElementById('squares');
+squaresContainer.onclick = (e) => {
+  handleSquaresClick(e);
+  let showAllButton = document.createElement('button');
+  showAllButton.innerHTML = 'Mostrar todos';
+  document.body.append(showAllButton);
 };
