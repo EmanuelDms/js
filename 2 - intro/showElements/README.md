8efafe01f38270c7ac9bd0b84711cae00ae566a5
Emanuel Maia <emanuel@hnwc.com.br>
2021-12-21 14:00:51 -0300

    feat: show and hide selected squares

---
+++ 2 - intro/showElements/README.md
@@ -0,0 +1,6 @@
+# Ocultar e mostrar quadrados!
+## Funcionalidades
+- [x] Ocultar e mostrar todos os quadrados
+- [x] Apenas quadrados selecionados
+  - [x] Mostrar
+  - [x] Ocultar
--- 2 - intro/showElements/script.js
+++ 2 - intro/showElements/script.js
@@ -1,56 +1,75 @@
 const SHOW = 1;
 const HIDE = 0;
 
-/**
- * * Ao clicar em um quadrado, o mesmo fica selecionado
- */
 function handleSquaresClick(e) {
   const [selectedSquare] = e.path;
-  const isSquareClicked = selectedSquare.style.border !== 'none';
-  if (!isSquareClicked) {
-    selectedSquare.style.border = '1px solid black';    
+
+  const isParentElement = selectedSquare.childElementCount > 0;
+  if (isParentElement) return;
+
+  const isSquareClicked = selectedSquare.style.border === 'none';
+  let newStatus = 0;
+  let newBorder = 'none';
+
+  if (isSquareClicked) {
+    newStatus = 1;
+    newBorder = '1px solid black';
   };
 
-  return selectedSquare;
-}
+  selectedSquare.innerHTML = newStatus;
+  selectedSquare.style.border = newBorder;
+};
 
 function defaultSettings() {
   let squares = document.getElementsByClassName('square');
   Object.entries(squares).forEach(([, square], index) => {
     square.style.border = 'none';
-    return square.style.display = 'inline-block';
+    square.style.opacity = 1;
+    square.innerHTML = 0;
+    square.style.cursor = 'pointer';
   });
   let handleVisibilityButton = document.getElementById('handleVisibilityButton');
-  handleVisibilityButton.innerHTML = 'Ocultar quadrado';
+  handleVisibilityButton.innerHTML = 'Ocultar quadrado(s)';
   handleVisibilityButton.value = SHOW;
+  let quantityOfSelectedSquares = document.getElementById('quantityOfSelectedSquares');
+  quantityOfSelectedSquares.value = 0;
 };
-
-/**
- * * Ao clicar em ocultar, somente ele ser afetado.
- * * Após isso, ser criado botão de mostrar todos
- */
 
 defaultSettings();
 
 let handleVisibilityButton = document.getElementById('handleVisibilityButton');
 handleVisibilityButton.onclick = () => {
   let squares = document.getElementsByClassName('square');
-  let handleVisibilityButton = document.getElementById('handleVisibilityButton');
   let handleVisibilityButtonStatus = Boolean(Number(handleVisibilityButton.value));
+  let hasSelectedSquares = false;
 
   if (handleVisibilityButtonStatus) {
     Object.entries(squares).forEach(([, square], index) => {
-      return square.style.display = 'none';
+      const squareStatus = Boolean(Number(square.innerHTML));
+      square.style.cursor = 'default';
+            
+      if (squareStatus) {
+        let quantityOfSelectedSquares = document.getElementById('quantityOfSelectedSquares');
+        quantityOfSelectedSquares.value = Number(quantityOfSelectedSquares.value) + 1;
+        square.style.border = 'none';
+        square.style.opacity = 0.5;
+        hasSelectedSquares = true;
+      };
     });
+
+    if (!hasSelectedSquares) {
+      Object.entries(squares).forEach(([, square], index) => {
+        square.style.opacity = 0.5;
+      });
+    };
+
     handleVisibilityButton.value = HIDE;
-    handleVisibilityButton.innerHTML = 'Mostrar quadrado';
+    handleVisibilityButton.innerHTML = 'Mostrar quadrado(s)';
   } else defaultSettings();
 };
 
 let squaresContainer = document.getElementById('squares');
 squaresContainer.onclick = (e) => {
-  handleSquaresClick(e);
-  let showAllButton = document.createElement('button');
-  showAllButton.innerHTML = 'Mostrar todos';
-  document.body.append(showAllButton);
-};
+  let handleVisibilityButtonStatus = Boolean(Number(handleVisibilityButton.value));
+  if (handleVisibilityButtonStatus) handleSquaresClick(e);  
+}; 
